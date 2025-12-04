a3a2126b51e36a6d30aefe4f47efda447e708345
S.Rahul <s.rahul5116@outlook.com>
2025-12-04 21:06:27 +0530

    Basic File Added

---
+++ basic.ts
@@ -0,0 +1,17 @@
+// Types -> types -> dataTypes
+
+// TypeScript Dosen't run -> Process -> JS Runs It
+
+// Type Checker 
+
+// : -> defines Types , Always use ` ` in Code
+
+function greet(name: string): string {
+
+    return `Hello ${name}`;
+
+}
+
+console.log(greet("rahul"));
+// console.log(greet(42));
+
