79141565347655633748ffad348f3995f7815a86
S.Rahul <s.rahul5116@outlook.com>
2025-12-06 21:25:20 +0530

    Types In Ts Files Added

---
+++ TypeScriptSetup/src/typesInTs.ts
@@ -0,0 +1,17 @@
+// 2 types of Errors => 1. Sytax error ANd Type Errors
+// Inference : TypeScript is able to figure out the type of variable by itself
+
+let drink = "Coffie";
+
+let cups = Math.random() > 0.5 ? 10 : 5
+
+let channelName ="RahulAndCode";
+
+// Annotation : Manually specifying the type of variable, 
+
+let chaiFlavour :string ="Masala Chai"
+chaiFlavour = 'sdsds'
+
+let chaiOrder:number =232332;
+
+let chaiOrderBoolean:boolean = true;
