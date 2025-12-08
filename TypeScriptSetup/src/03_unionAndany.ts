ceee917a8a3dbc7f5297aeffc528508a4b4faa14
S.Rahul <s.rahul5116@outlook.com>
2025-12-08 16:16:28 +0530

    Add example of using union types with orders in 03_unionAndany.ts

--- TypeScriptSetup/src/03_unionAndany.ts
+++ TypeScriptSetup/src/03_unionAndany.ts
@@ -9,3 +9,16 @@ apiRequestStatus = "sucess"
 let airlineSeat :'aisle' |'window' | 'middle' ='middle'
 
 airlineSeat='aisle'
+
+const orders =['12','20','28','42']
+
+let currentorder : string | undefined ;
+
+for(let order of orders){
+    if(order=='28'){
+        currentorder =  order
+        break
+    }
+}
+
+console.log(currentorder);
