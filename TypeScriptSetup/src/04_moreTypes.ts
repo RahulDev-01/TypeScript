e49c76a16b743b52c7f8c7cd94f174367fd20c4a
S.Rahul <s.rahul5116@outlook.com>
2025-12-10 17:44:28 +0530

    Add type assertions and demonstrate usage of unknown type

--- TypeScriptSetup/src/04_moreTypes.ts
+++ TypeScriptSetup/src/04_moreTypes.ts
@@ -15,5 +15,21 @@ let bookObject =JSON.parse(bookString) as book
 
 console.log(bookObject);
 
-
+// Type Assertion : 
 const inputElement = document.getElementById("username") as HTMLInputElement
+
+let value: any 
+
+value ="chai"
+value=[1,2,3]
+value =2.5
+value.toUpperCase()
+
+let newValue: unknown 
+
+value ="chai"
+value=[1,2,3]
+value =2.5
+if(typeof newValue==="string"){
+    newValue.toUpperCase();
+}
