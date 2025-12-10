e354e87d2250c535e63581948b89f7ab53ae3e83
S.Rahul <s.rahul5116@outlook.com>
2025-12-10 17:39:05 +0530

    Add initial implementation of response handling and book type definition

---
+++ TypeScriptSetup/src/04_moreTypes.ts
@@ -0,0 +1,12 @@
+let response:any ="42";
+
+let numericLength : number = (response as string).length
+
+type book ={
+    name:string
+
+}
+
+let bookString = '{"name":"One Thing"}';
+
+let bookObject 
