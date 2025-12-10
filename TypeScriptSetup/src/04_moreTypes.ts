6ce186bbb820a49e8dfcd863e487f4e93006c966
S.Rahul <s.rahul5116@outlook.com>
2025-12-10 17:41:27 +0530

    Refactor book object parsing and add console log for output

--- TypeScriptSetup/src/04_moreTypes.ts
+++ TypeScriptSetup/src/04_moreTypes.ts
@@ -1,12 +1,19 @@
+
 let response:any ="42";
 
 let numericLength : number = (response as string).length
 
 type book ={
+
     name:string
 
 }
 
 let bookString = '{"name":"One Thing"}';
 
-let bookObject 
+let bookObject =JSON.parse(bookString) as book
+
+console.log(bookObject);
+
+
+const inputElement = document.getElementById("username") as HTMLInputElement
