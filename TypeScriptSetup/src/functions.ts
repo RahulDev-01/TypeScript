71b2e7f82848ab03a229801cee5c25a761a131be
S.Rahul <s.rahul5116@outlook.com>
2025-12-12 22:03:07 +0530

    Add makeChai function and getChaiPrice function implementation

---
+++ TypeScriptSetup/src/functions.ts
@@ -0,0 +1,11 @@
+function makeChai(type:string, cups:number){
+    console.log(`Making ${cups} cups of ${type}`);
+    
+}
+
+makeChai("Masala",2)
+
+
+function getChaiPrice():number{
+    return 25
+}
