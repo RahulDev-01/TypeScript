9f11367e4d2b612b38fbd4fc484028a52f7bc6c2
S.Rahul <s.rahul5116@outlook.com>
2025-12-12 21:42:14 +0530

    Add type narrowing functions and examples for chai preparation and serving

---
+++ TypeScriptSetup/src/interfaceTs.ts
@@ -0,0 +1,10 @@
+function makeChai(order:{typr:string; sugar:number;strong:boolean}){
+    console.log(order);
+    
+}
+
+
+function serveChai(order:{type:string;sugar:number;strong:boolean}){
+    console.log(order);
+    
+}
