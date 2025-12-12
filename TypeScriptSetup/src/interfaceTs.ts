93998d3ff317b454bfc7fe34592f1d92c40a8c5f
S.Rahul <s.rahul5116@outlook.com>
2025-12-12 21:56:30 +0530

    Add Response, TeaType, and User types; implement orderChai function and MasalaChaiType definition

--- TypeScriptSetup/src/interfaceTs.ts
+++ TypeScriptSetup/src/interfaceTs.ts
@@ -26,11 +26,59 @@ class MasalaChai implements TeaRecipe {
     milk=50;
 }
 
-// interface 
+// Interface in Strings
+
 interface CupSize {
     size:"small"|"large" 
 }
 
 class Chai implements CupSize{
  size: "small" | "large" ='large';
+}
+
+type Response ={ok:true} | {ok:false}
+
+// class myRes implements  Response{
+//     ok: boolean=true;
+// }
+
+// | -> Union 
+type TeaType ="masla"|"ginger"|"lemon"
+
+function orderChai(t:TeaType){
+    console.log(t);
+    
+}
+// & -> Intersection  
+type BaseChai ={teaLeaves:number}
+type Extra={masala:number}
+
+// Intersection & 
+type MasalaChaiType = BaseChai & Extra
+
+const cup : MasalaChaiType={
+    teaLeaves:5,
+    masala:10
+}
+// Optional Property  ?
+type User ={
+    username:string;
+    bio?:string;
+}
+const user1:User={
+    username:"Rahul"
+}
+const user2:User={
+    username:"Rahul",
+    bio:"I love chai"
+}
+
+type Config={
+    readonly appName:string;
+    version:number;
+}
+
+const config:Config={
+    appName:"ChaiApp",
+    version:1.0
 }
